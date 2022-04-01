165378c110873e6515ea57eecc94ecb4c28f010a
thanhvodev <75848160+thanhvodev@users.noreply.github.com>
2022-04-01 23:09:37 +0700

    use write instead of append

--- app/src/main/java/com/google/samples/quickstart/signin/InputInfomation.java
+++ app/src/main/java/com/google/samples/quickstart/signin/InputInfomation.java
@@ -23,6 +23,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.JsonReader;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -35,12 +36,16 @@ import com.google.firebase.database.Transaction;
 import com.google.firebase.database.ValueEventListener;
 import com.google.samples.quickstart.signin.databinding.ActivityInputInfomationBinding;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Objects;
 
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -63,7 +68,7 @@ public class InputInfomation extends AppCompatActivity {
 
     //For doing request once per day
     private SharedPreferences sp;
-    private static int lineNumber;
+
     // Write a message to the database
 //    FirebaseDatabase database;
 //    DatabaseReference myRef;
@@ -130,8 +135,9 @@ public class InputInfomation extends AppCompatActivity {
     private void handleOK() {
             new Thread(() -> {
                 try {
-                    appendData();
-                } catch (IOException e) {
+                    int lineNumber =  getLineNumber();
+                    appendData(lineNumber);
+                } catch (IOException | JSONException e) {
                     e.printStackTrace();
                 }
             }).start();
@@ -145,12 +151,12 @@ public class InputInfomation extends AppCompatActivity {
         editor.putBoolean("isFirstRun", isFirstRun);
         editor.apply();
     }
-    @SuppressLint("NewApi")
-    private static String getDate() {
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        LocalDate now = LocalDate.now();
-        return dtf.format(now);
-    }
+//    @SuppressLint("NewApi")
+//    private static String getDate() {
+//        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+//        LocalDate now = LocalDate.now();
+//        return dtf.format(now);
+//    }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     private static String getTime() {
@@ -163,7 +169,7 @@ public class InputInfomation extends AppCompatActivity {
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
         MediaType mediaType = MediaType.parse("text/plain");
-        RequestBody body = RequestBody.create(mediaType, "{\r\n  \"requests\": [\r\n    {\r\n      \"addSheet\": {\r\n        \"properties\": {\r\n          \"title\": \"" + "Output Report" + "\",\r\n          \"gridProperties\": {\r\n            \"rowCount\": 1000,\r\n            \"columnCount\": 26\r\n          },\r\n        }\r\n      }\r\n    }\r\n  ]\r\n}");
+        RequestBody body = RequestBody.create(mediaType, "{\r\n  \"requests\": [\r\n    {\r\n      \"addSheet\": {\r\n        \"properties\": {\r\n          \"title\": \"" + "OutputReport" + "\",\r\n          \"gridProperties\": {\r\n            \"rowCount\": 1000,\r\n            \"columnCount\": 26\r\n          },\r\n        }\r\n      }\r\n    }\r\n  ]\r\n}");
         Request request = new Request.Builder()
                 .url("https://sheets.googleapis.com/v4/spreadsheets/" + spreadSheetID + ":batchUpdate")
                 .method("POST", body)
@@ -180,7 +186,7 @@ public class InputInfomation extends AppCompatActivity {
         MediaType mediaType = MediaType.parse("text/plain");
         RequestBody body = RequestBody.create(mediaType, "{\r\n  \"values\": [\r\n    [\r\n      \"Thời gian quét\",\r\n      \"Người quét\",\r\n      \"Số Đơn\",\r\n      \"Máy\",\r\n      \"Tên hàng\",\r\n      \"Số tấm\",\r\n      \"Trống\",\r\n      \"Ghi chú\",\r\n      \"Loại hàng\",\r\n      \"Loại công đoạn\"\n    ],\r\n  ]\r\n}");
         Request request = new Request.Builder()
-                .url("https://sheets.googleapis.com/v4/spreadsheets/" + spreadSheetID + "/values/" + "Output Report" + "!A1%3AJ1?includeValuesInResponse=true&responseDateTimeRenderOption=FORMATTED_STRING&responseValueRenderOption=FORMATTED_VALUE&valueInputOption=USER_ENTERED&key=" + API_KEY)
+                .url("https://sheets.googleapis.com/v4/spreadsheets/" + spreadSheetID + "/values/" + "OutputReport" + "!A1%3AJ1?includeValuesInResponse=true&responseDateTimeRenderOption=FORMATTED_STRING&responseValueRenderOption=FORMATTED_VALUE&valueInputOption=USER_ENTERED&key=" + API_KEY)
                 .method("PUT", body)
                 .addHeader("Authorization", "Bearer " + accessToken)
                 .addHeader("Content-Type", "text/plain")
@@ -188,45 +194,18 @@ public class InputInfomation extends AppCompatActivity {
         Response response = client.newCall(request).execute();
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    private void appendData() throws IOException {
-
-        String loaiHang = binding.don.isChecked() ? "Đơn" : "Tái chế";
-        String loaiCongDoan = binding.chuan.isChecked() ? "Chuẩn" : "Thêm";
-        OkHttpClient client = new OkHttpClient().newBuilder()
-                .build();
-        MediaType mediaType = MediaType.parse("text/plain");
-        RequestBody body = RequestBody.create(mediaType, "{\r\n  \"values\": [\r\n    [\r\n      \"" + getTime() + "\",\r\n      \"" + username + "\",\r\n      \" " + soDon + "\",\r\n      \"" + may + "\",\r\n      \"" + binding.tenhangValue.getText().toString() + "\",\r\n      \"" + binding.sotamValue.getText().toString() + "\",\r\n      \"" + binding.trongValue.getText().toString() + "\",\r\n      \"" + binding.ghichuValue.getText().toString() + "\",\r\n      \"" + loaiHang + "\",\r\n      \"" + loaiCongDoan + "\"\r\n    ]\r\n  ]\r\n}");
-        Request request = new Request.Builder()
-                .url("https://sheets.googleapis.com/v4/spreadsheets/" + spreadSheetID + "/values/Output Report!A1:J1:append?includeValuesInResponse=true&insertDataOption=INSERT_ROWS&responseDateTimeRenderOption=FORMATTED_STRING&responseValueRenderOption=FORMATTED_VALUE&valueInputOption=USER_ENTERED&key=" + API_KEY)
-                .method("POST", body)
-                .addHeader("Authorization", "Bearer " + accessToken)
-                .addHeader("Content-Type", "text/plain")
-                .build();
-        Response response = client.newCall(request).execute();
-
-        if (response.code() == 200) {
-            showToastSuccess("Thêm thành công");
-        } else {
-            setIsFirstRun(true);
-            showToastFail("Thêm số đơn '" +soDon+ "' thất bại!");
-        }
-
-    }
-
 //    @RequiresApi(api = Build.VERSION_CODES.O)
 //    private void appendData() throws IOException {
-//// Read from the database
+//
 //        String loaiHang = binding.don.isChecked() ? "Đơn" : "Tái chế";
 //        String loaiCongDoan = binding.chuan.isChecked() ? "Chuẩn" : "Thêm";
-//
 //        OkHttpClient client = new OkHttpClient().newBuilder()
 //                .build();
 //        MediaType mediaType = MediaType.parse("text/plain");
-//        RequestBody body = RequestBody.create(mediaType, "{\r\n  \"values\": [\r\n    [\r\n      \"" + getTime() + "\",\r\n      \"" + username + "\",\r\n      \"" + soDon + "\",\r\n      \"" + may + "\",\r\n      \"" + binding.tenhangValue.getText().toString() + "\",\r\n      \""+ binding.sotamValue.getText().toString() + "\",\r\n      \"" + binding.trongValue.getText().toString() + "\",\r\n      \"" + binding.ghichuValue.getText().toString() + "\",\r\n      \"" + loaiHang + "\",\r\n      \"" + loaiCongDoan + "\"\n    ],\r\n  ]\r\n}");
+//        RequestBody body = RequestBody.create(mediaType, "{\r\n  \"values\": [\r\n    [\r\n      \"" + getTime() + "\",\r\n      \"" + username + "\",\r\n      \" " + soDon + "\",\r\n      \"" + may + "\",\r\n      \"" + binding.tenhangValue.getText().toString() + "\",\r\n      \"" + binding.sotamValue.getText().toString() + "\",\r\n      \"" + binding.trongValue.getText().toString() + "\",\r\n      \"" + binding.ghichuValue.getText().toString() + "\",\r\n      \"" + loaiHang + "\",\r\n      \"" + loaiCongDoan + "\"\r\n    ]\r\n  ]\r\n}");
 //        Request request = new Request.Builder()
-//                .url("https://sheets.googleapis.com/v4/spreadsheets/" + spreadSheetID + "/values/" + "Output Report" + "!A" + lineNumber + "%3AJ"+lineNumber +"?includeValuesInResponse=true&responseDateTimeRenderOption=FORMATTED_STRING&responseValueRenderOption=FORMATTED_VALUE&valueInputOption=USER_ENTERED&key=" + API_KEY)
-//                .method("PUT", body)
+//                .url("https://sheets.googleapis.com/v4/spreadsheets/" + spreadSheetID + "/values/OutputReport!A1:J1:append?includeValuesInResponse=true&insertDataOption=INSERT_ROWS&responseDateTimeRenderOption=FORMATTED_STRING&responseValueRenderOption=FORMATTED_VALUE&valueInputOption=USER_ENTERED&key=" + API_KEY)
+//                .method("POST", body)
 //                .addHeader("Authorization", "Bearer " + accessToken)
 //                .addHeader("Content-Type", "text/plain")
 //                .build();
@@ -234,12 +213,61 @@ public class InputInfomation extends AppCompatActivity {
 //
 //        if (response.code() == 200) {
 //            showToastSuccess("Thêm thành công");
-//            myRef.setValue(lineNumber+1);
 //        } else {
 //            setIsFirstRun(true);
 //            showToastFail("Thêm số đơn '" +soDon+ "' thất bại!");
 //        }
+//
 //    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private int getLineNumber() throws IOException, JSONException {
+
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        Request request = new Request.Builder()
+                .url("https://sheets.googleapis.com/v4/spreadsheets/"+ spreadSheetID +"/values/OutputReport!B:B?majorDimension=COLUMNS&key=" + API_KEY)
+                .method("GET", null)
+                .addHeader("Authorization", "Bearer "+accessToken)
+                .build();
+        Response response = client.newCall(request).execute();
+
+        if (response.code() == 200) {
+            JSONObject obj = new JSONObject(Objects.requireNonNull(response.body()).string());
+//            Log.d("Log_error", String.valueOf(obj.getJSONArray("values").get(0).toString().split(",").length+2));
+            return obj.getJSONArray("values").get(0).toString().split(",").length+1;
+        } else {
+            return -1;
+        }
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void appendData(int lineNumber) throws IOException {
+// Read from the database
+        String loaiHang = binding.don.isChecked() ? "Đơn" : "Tái chế";
+        String loaiCongDoan = binding.chuan.isChecked() ? "Chuẩn" : "Thêm";
+
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("text/plain");
+        RequestBody body = RequestBody.create(mediaType, "{\r\n  \"values\": [\r\n    [\r\n      \"" + getTime() + "\",\r\n      \"" + username + "\",\r\n      \"" + soDon + "\",\r\n      \"" + may + "\",\r\n      \"" + binding.tenhangValue.getText().toString() + "\",\r\n      \""+ binding.sotamValue.getText().toString() + "\",\r\n      \"" + binding.trongValue.getText().toString() + "\",\r\n      \"" + binding.ghichuValue.getText().toString() + "\",\r\n      \"" + loaiHang + "\",\r\n      \"" + loaiCongDoan + "\"\n    ],\r\n  ]\r\n}");
+        Request request = new Request.Builder()
+                .url("https://sheets.googleapis.com/v4/spreadsheets/" + spreadSheetID + "/values/" + "OutputReport" + "!A" + lineNumber + "%3AJ"+lineNumber +"?includeValuesInResponse=true&responseDateTimeRenderOption=FORMATTED_STRING&responseValueRenderOption=FORMATTED_VALUE&valueInputOption=USER_ENTERED&key=" + API_KEY)
+                .method("PUT", body)
+                .addHeader("Authorization", "Bearer " + accessToken)
+                .addHeader("Content-Type", "text/plain")
+                .build();
+        Response response = client.newCall(request).execute();
+
+        if (response.code() == 200) {
+            showToastSuccess("Thêm thành công");
+//            myRef.setValue(lineNumber+1);
+        } else {
+            setIsFirstRun(true);
+            showToastFail("Thêm số đơn '" +soDon+ "' thất bại!");
+        }
+    }
 
     public void showToastSuccess(final String toast) {
         runOnUiThread(() -> {
@@ -256,25 +284,25 @@ public class InputInfomation extends AppCompatActivity {
     }
 
 
-    private void addNotification() {
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
-        Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+//    private void addNotification() {
+//        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+//        Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+//
+//        Notification notification = new NotificationCompat.Builder(this, NotificationFailAlert.CHANNEL_ID)
+//                .setContentTitle("THÊM THẤT BẠI")
+//                .setContentText("Thêm số đơn '" +soDon+ "', máy '"+ may +"' thất bại! Hãy quét lại!")
+//                .setLargeIcon(bitmap)
+//                .setSmallIcon(R.mipmap.ic_launcher)
+//                .setSound(uri)
+//                .setColor(getResources().getColor(R.color.blue_grey_500))
+//                .build();
+//
+//        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+//        notificationManagerCompat.notify(getNotificationId(), notification);
+//
+//    }
 
-        Notification notification = new NotificationCompat.Builder(this, NotificationFailAlert.CHANNEL_ID)
-                .setContentTitle("THÊM THẤT BẠI")
-                .setContentText("Thêm số đơn '" +soDon+ "', máy '"+ may +"' thất bại! Hãy quét lại!")
-                .setLargeIcon(bitmap)
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setSound(uri)
-                .setColor(getResources().getColor(R.color.blue_grey_500))
-                .build();
-
-        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
-        notificationManagerCompat.notify(getNotificationId(), notification);
-
-    }
-
-    private int getNotificationId() {
-        return (int) new Date().getTime();
-    }
+//    private int getNotificationId() {
+//        return (int) new Date().getTime();
+//    }
 }
